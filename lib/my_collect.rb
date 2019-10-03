10d081e8e3bdf3f9771a055fb81cc7e96e98343d
Susan Meyers <smmeyers24@gmail.com>
2019-10-03 20:07:06 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
+require 'pry'
+# def my_collect(array)
+#   collection = []
+#   array.collect do |language|
+#     #binding.pry
+#     collection << language.upcase
+#   end
+#     collection
+# end
 
+def my_collect(array)
+  collection = Array.new
+  i = 0
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
 
