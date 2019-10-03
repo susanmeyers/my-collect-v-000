6fdf8d1116ba37f2b6075fda79713cc1c5480e52
Susan Meyers <smmeyers24@gmail.com>
2019-10-03 19:58:53 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,12 @@
 require 'pry'
-def my_collect(array)
-  collection = []
-  array.collect do |language|
-    #binding.pry
-    collection << language.upcase
-  end
-    collection
-end
+# def my_collect(array)
+#   collection = []
+#   array.collect do |language|
+#     #binding.pry
+#     collection << language.upcase
+#   end
+#     collection
+# end
 
 def my_collect(array)
   collection = Array.new
