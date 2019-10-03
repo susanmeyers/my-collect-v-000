e497482160e35a9bb57fe463b449cab716a03f0f
Susan Meyers <smmeyers24@gmail.com>
2019-10-03 19:32:07 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,7 @@
 # require 'pry"
 def my_collect(array)
   collection = []
-  array.collect do |language| language.upcase|
+  array.collect do |language, language.upcase|
     collection << array
   end
   
